016f6fb2a8ca990fae63da5f73365a841f9bacd8
Dennis Torres <dennistorres.torres@portoseguro.com.br>
2024-04-02 18:56:30 -0300

    Adding icons. Adjusting podfile. Starting Home MVVM-C.

--- Podfile
+++ Podfile
@@ -1,11 +1,19 @@
 # Uncomment the next line to define a global platform for your project
-# platform :ios, '17.4'
+platform :ios, '17.4'
+use_frameworks!
+
+def setup_libs
+  pod 'SwiftGen', '~> 6.0'
+end
+
+def external_libs
+  pod 'SnapKit', '~> 5.6.0'
+end
 
 target 'TreinamentoBreweryBees' do
   # Comment the next line if you don't want to use dynamic frameworks
-  use_frameworks!
-  
-  pod 'SwiftGen', '~> 6.0'
+  setup_libs
+  external_libs
   
 end
 
--- TreinamentoBreweryBees/Source/Features/MVVMC/Home/Controller/HomeViewController.swift
+++ TreinamentoBreweryBees/Source/Features/MVVMC/Home/Controller/HomeViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    // MARK: - Constants
+    
+    private enum Constants {
+        static let mainViewColor: UIColor = .white
+        static let titleHeight: CGFloat = 28
+        static let defaultSpacing: CGFloat = 16
+    }
+    
+    private lazy var mainStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.distribution = .fillProportionally
+        stackView.alignment = .fill
+        stackView.spacing = Constants.defaultSpacing
+        stackView.backgroundColor = .white
+        return stackView
+    }()
 
+    // MARK: - Instantiate
+    
+    /*public static func instantiate(viewModel: HomeViewModelProtocol) -> HomeViewController {
+        let controller = HomeViewController()
+        controller.viewModel = viewModel
+        return controller
+    }*/
+    
+    // MARK: - Life Cycles/
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        view.backgroundColor = .green
+        setupView()
+    }
+    
+    // MARK: - Private methods
+    
+    private func setupView() {
+        view.backgroundColor = Constants.mainViewColor
+        
         title = "\(TreinamentoBreweryBeesLocalizable.userName.localized): Dennis"
     }
 }
